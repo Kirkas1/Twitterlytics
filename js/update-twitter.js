64b2cebe6813d5c8728584074e8efd8c9ef6e257
Kirkas1 <ikirk870@gmail.com>
2015-12-05 17:48:12 -0500

    Finnius' changes

--- js/update-twitter.js
+++ js/update-twitter.js
@@ -7,6 +7,12 @@ function updateTwitter() {
 		$("div#hood-timeline").hide();
 		$("div#umd-timeline").hide();
 		$("div#umbc-timeline").show();
+		$("canvas#umbccanvas").show();
+		$("canvas#umdcanvas").hide();
+		$("canvas#hopkinscanvas").hide();
+		$("canvas#towsoncanvas").hide();
+		$("canvas#hoodcanvas").hide();
+		
 	} 
 	else if (school=="College Park") {
 		$("div#towson-timeline").hide();
@@ -14,6 +20,11 @@ function updateTwitter() {
 		$("div#hood-timeline").hide();
 		$("div#umbc-timeline").hide();
 		$("div#umd-timeline").show();
+		$("canvas#umbccanvas").hide();
+		$("canvas#umdcanvas").show();
+		$("canvas#hopkinscanvas").hide();
+		$("canvas#towsoncanvas").hide();
+		$("canvas#hoodcanvas").hide();
 	} 
 	else if (school=="Towson") {
 		$("div#jhu-timeline").hide();
@@ -21,6 +32,11 @@ function updateTwitter() {
 		$("div#umd-timeline").hide();
 		$("div#umbc-timeline").hide();
 		$("div#towson-timeline").show();
+		$("canvas#umbccanvas").hide();
+		$("canvas#umdcanvas").hide();
+		$("canvas#hopkinscanvas").hide();
+		$("canvas#towsoncanvas").show();
+		$("canvas#hoodcanvas").hide();
 	} 
 	else if (school=="John Hopkins") {
 		$("div#towson-timeline").hide();
@@ -28,6 +44,11 @@ function updateTwitter() {
 		$("div#umd-timeline").hide();
 		$("div#umbc-timeline").hide();
 		$("div#jhu-timeline").show();
+		$("canvas#umbccanvas").hide();
+		$("canvas#umdcanvas").hide();
+		$("canvas#hopkinscanvas").show();
+		$("canvas#towsoncanvas").hide();
+		$("canvas#hoodcanvas").hide();
 	} 
 	else if (school=="Hood") {
 		$("div#towson-timeline").hide();
@@ -35,5 +56,10 @@ function updateTwitter() {
 		$("div#umd-timeline").hide();
 		$("div#umbc-timeline").hide();
 		$("div#hood-timeline").show();
+		$("canvas#umbccanvas").hide();
+		$("canvas#umdcanvas").hide();
+		$("canvas#hopkinscanvas").hide();
+		$("canvas#towsoncanvas").hide();
+		$("canvas#hoodcanvas").show();
 	}
 }
