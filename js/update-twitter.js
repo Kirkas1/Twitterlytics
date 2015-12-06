a87fd59051b37da22be66a46fee2b0a0a3624a6b
Kirkas1 <ikirk870@gmail.com>
2015-12-05 21:40:54 -0500

    Added subject select support

--- js/update-twitter.js
+++ js/update-twitter.js
@@ -7,12 +7,37 @@ function updateTwitter() {
 		$("div#hood-timeline").hide();
 		$("div#umd-timeline").hide();
 		$("div#umbc-timeline").show();
-		$("canvas#umbccanvas").show();
-		$("canvas#umdcanvas").hide();
-		$("canvas#hopkinscanvas").hide();
-		$("canvas#towsoncanvas").hide();
-		$("canvas#hoodcanvas").hide();
+
+		$("canvas#umd-canvas").hide();
+		$("canvas#umd-food-canvas").hide();
+		$("canvas#umd-acad-canvas").hide();
+		$("canvas#hopkins-canvas").hide();
+		$("canvas#hopkins-food-canvas").hide();
+		$("canvas#hopkins-acad-canvas").hide();
+		$("canvas#towson-canvas").hide();
+		$("canvas#towson-food-canvas").hide();
+		$("canvas#towson-acad-canvas").hide();
+		$("canvas#hood-canvas").hide();
+		$("canvas#hood-food-canvas").hide();
+		$("canvas#hood-acad-canvas").hide();
+
+		if(document.getElementById('subject-food').checked) {
+			$("canvas#umbc-canvas").hide();
+			$("canvas#umbc-acad-canvas").hide();
+			$("canvas#umbc-food-canvas").show();
+		} else if(document.getElementById('subject-acad').checked) {
+			$("canvas#umbc-canvas").hide();
+			$("canvas#umbc-food-canvas").hide();
+			$("canvas#umbc-acad-canvas").show();
+		}
+		else {
+			$("canvas#umbc-food-canvas").hide();
+			$("canvas#umbc-acad-canvas").hide();
+			$("canvas#umbc-canvas").show();
+		}
 		
+		map.panTo(new google.maps.LatLng(39.2555, -76.7111));
+		map.setZoom(17);
 	} 
 	else if (school=="College Park") {
 		$("div#towson-timeline").hide();
@@ -20,11 +45,36 @@ function updateTwitter() {
 		$("div#hood-timeline").hide();
 		$("div#umbc-timeline").hide();
 		$("div#umd-timeline").show();
-		$("canvas#umbccanvas").hide();
-		$("canvas#umdcanvas").show();
-		$("canvas#hopkinscanvas").hide();
-		$("canvas#towsoncanvas").hide();
-		$("canvas#hoodcanvas").hide();
+		
+		$("canvas#umbc-food-canvas").hide();
+		$("canvas#umbc-acad-canvas").hide();
+		$("canvas#hopkins-canvas").hide();
+		$("canvas#hopkins-food-canvas").hide();
+		$("canvas#hopkins-acad-canvas").hide();
+		$("canvas#towson-canvas").hide();
+		$("canvas#towson-food-canvas").hide();
+		$("canvas#towson-acad-canvas").hide();
+		$("canvas#hood-canvas").hide();
+		$("canvas#hood-food-canvas").hide();
+		$("canvas#hood-acad-canvas").hide();
+
+		if(document.getElementById('subject-food').checked) {
+			$("canvas#umd-canvas").hide();
+			$("canvas#umd-acad-canvas").hide();
+			$("canvas#umd-food-canvas").show();
+		} else if(document.getElementById('subject-acad').checked) {
+			$("canvas#umd-canvas").hide();
+			$("canvas#umd-food-canvas").hide();
+			$("canvas#umd-acad-canvas").show();
+		}
+		else {
+			$("canvas#umd-food-canvas").hide();
+			$("canvas#umd-acad-canvas").hide();
+			$("canvas#umd-canvas").show();
+		}
+
+		map.panTo(new google.maps.LatLng(38.988741, -76.943178));
+		map.setZoom(15);
 	} 
 	else if (school=="Towson") {
 		$("div#jhu-timeline").hide();
@@ -32,11 +82,37 @@ function updateTwitter() {
 		$("div#umd-timeline").hide();
 		$("div#umbc-timeline").hide();
 		$("div#towson-timeline").show();
-		$("canvas#umbccanvas").hide();
-		$("canvas#umdcanvas").hide();
-		$("canvas#hopkinscanvas").hide();
-		$("canvas#towsoncanvas").show();
-		$("canvas#hoodcanvas").hide();
+		
+		$("canvas#umbc-canvas").hide();
+		$("canvas#umbc-food-canvas").hide();
+		$("canvas#umbc-acad-canvas").hide();
+		$("canvas#umd-canvas").hide();
+		$("canvas#umd-food-canvas").hide();
+		$("canvas#umd-acad-canvas").hide();
+		$("canvas#hopkins-canvas").hide();
+		$("canvas#hopkins-food-canvas").hide();
+		$("canvas#hopkins-acad-canvas").hide();
+		$("canvas#hood-canvas").hide();
+		$("canvas#hood-food-canvas").hide();
+		$("canvas#hood-acad-canvas").hide();
+		
+		if(document.getElementById('subject-food').checked) {
+			$("canvas#towson-canvas").hide();
+			$("canvas#towson-acad-canvas").hide();
+			$("canvas#towson-food-canvas").show();
+		} else if(document.getElementById('subject-acad').checked) {
+			$("canvas#towson-canvas").hide();
+			$("canvas#towson-food-canvas").hide();
+			$("canvas#towson-acad-canvas").show();
+		}
+		else {
+			$("canvas#towson-food-canvas").hide();
+			$("canvas#towson-acad-canvas").hide();
+			$("canvas#towson-canvas").show();
+		}
+
+		map.panTo(new google.maps.LatLng(39.394360, -76.610733));
+		map.setZoom(16);
 	} 
 	else if (school=="John Hopkins") {
 		$("div#towson-timeline").hide();
@@ -44,11 +120,37 @@ function updateTwitter() {
 		$("div#umd-timeline").hide();
 		$("div#umbc-timeline").hide();
 		$("div#jhu-timeline").show();
-		$("canvas#umbccanvas").hide();
-		$("canvas#umdcanvas").hide();
-		$("canvas#hopkinscanvas").show();
-		$("canvas#towsoncanvas").hide();
-		$("canvas#hoodcanvas").hide();
+
+		$("canvas#umbc-canvas").hide();
+		$("canvas#umbc-food-canvas").hide();
+		$("canvas#umbc-acad-canvas").hide();
+		$("canvas#umd-canvas").hide();
+		$("canvas#umd-food-canvas").hide();
+		$("canvas#umd-acad-canvas").hide();
+		$("canvas#towson-canvas").hide();
+		$("canvas#towson-food-canvas").hide();
+		$("canvas#towson-acad-canvas").hide();
+		$("canvas#hood-canvas").hide();
+		$("canvas#hood-food-canvas").hide();
+		$("canvas#hood-acad-canvas").hide();
+
+		if(document.getElementById('subject-food').checked) {
+			$("canvas#hopkins-canvas").hide();
+			$("canvas#hopkins-acad-canvas").hide();
+			$("canvas#hopkins-food-canvas").show();
+		} else if(document.getElementById('subject-acad').checked) {
+			$("canvas#hopkins-canvas").hide();
+			$("canvas#hopkins-food-canvas").hide();
+			$("canvas#hopkins-acad-canvas").show();
+		}
+		else {
+			$("canvas#hopkins-food-canvas").hide();
+			$("canvas#hopkins-acad-canvas").hide();
+			$("canvas#hopkins-canvas").show();
+		}
+
+		map.panTo(new google.maps.LatLng(44.926629, -93.404636));
+		map.setZoom(14);
 	} 
 	else if (school=="Hood") {
 		$("div#towson-timeline").hide();
@@ -56,10 +158,36 @@ function updateTwitter() {
 		$("div#umd-timeline").hide();
 		$("div#umbc-timeline").hide();
 		$("div#hood-timeline").show();
-		$("canvas#umbccanvas").hide();
-		$("canvas#umdcanvas").hide();
-		$("canvas#hopkinscanvas").hide();
-		$("canvas#towsoncanvas").hide();
-		$("canvas#hoodcanvas").show();
+		
+		$("canvas#umbc-canvas").hide();
+		$("canvas#umbc-food-canvas").hide();
+		$("canvas#umbc-acad-canvas").hide();
+		$("canvas#umd-canvas").hide();
+		$("canvas#umd-food-canvas").hide();
+		$("canvas#umd-acad-canvas").hide();
+		$("canvas#hopkins-canvas").hide();
+		$("canvas#hopkins-food-canvas").hide();
+		$("canvas#hopkins-acad-canvas").hide();
+		$("canvas#towson-canvas").hide();
+		$("canvas#towson-food-canvas").hide();
+		$("canvas#towson-acad-canvas").hide();
+
+		if(document.getElementById('subject-food').checked) {
+			$("canvas#hood-canvas").hide();
+			$("canvas#hood-acad-canvas").hide();
+			$("canvas#hood-food-canvas").show();
+		} else if(document.getElementById('subject-acad').checked) {
+			$("canvas#hood-canvas").hide();
+			$("canvas#hood-food-canvas").hide();
+			$("canvas#hood-acad-canvas").show();
+		}
+		else {
+			$("canvas#hood-food-canvas").hide();
+			$("canvas#hood-acad-canvas").hide();
+			$("canvas#hood-canvas").show();
+		}
+		
+		map.panTo(new google.maps.LatLng(39.421573, -77.419701));
+		map.setZoom(17);
 	}
 }
